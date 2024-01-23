16cb8019a8c1e9a51b7392b8554eeada93a88997
Tobi H <Tobizzzzle@gmail.com>
2024-01-23 17:19:11 +0100

    Revert "[DCU] Readme remove typos"
    This reverts commit 7d18ac19476c72360811bf26fe7be6a65bab3d57.

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from models import randomPlayer as rP
 
 
 #creating the player objects
-'''p1 = rP.Player('Stewie')
-p2 = rP.Player('Brian')'''
+p1 = rP.Player('Stewie')
+p2 = rP.Player('Brian')
 p3 = rP.Player('Peter')
 p4 = rP.Player('Lois')
 
 #creating the game object
-Game = PokerGame.Game(p3, p4, dealmode='manuel') #dealmode manuel/auto
+Game = PokerGame.Game(p1, p2, p3, p4, dealmode='auto') #dealmode manuel/auto
 
 #dealmode manuel takes cards for testing
-'''p1.cards = ['2s', '4s']
-p2.cards = ['2h', '4h']'''
-p3.cards = ['5c', '5c']
+p1.cards = ['2s', '3s']
+p2.cards = ['2h', '3h']
+p3.cards = ['2c', '3c']
 p4.cards = ['2d', '3d']
-Game.board = ['4d', '5d', '6d', '7d', '6c']
+Game.board = ['4c', '5h', 'Ac', 'Td', 'Kh']
 
 Game.start()
--- models/test.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("trainingData/hands_valid.json", "r") as read_file:
-    data = json.load(read_file)
-
-for game in data:
-    print(game['players'][0]['bets'][1]['actions'][0])
